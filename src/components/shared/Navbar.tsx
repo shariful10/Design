bef2e4c754021f568334f4f713dfeebb34340d82
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-07 13:45:26 +0600

    Desktop Navbar completed

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -6,25 +6,25 @@ import { LuUserPlus } from "react-icons/lu";
 
 const Navbar = () => {
 	return (
-		<div>
+		<div className="w-[95%] mx-auto py-3">
 			<div className="flex justify-between items-center">
 				<div className="flex items-center gap-4">
 					<Image src={logo} alt="" />
-					<div className="cursor-pointer flex gap-2">
-						<p className="hover:text-[#ff6700]">Cources</p>
-						<p className="hover:text-[#ff6700]">Pricing</p>
-						<p className="hover:text-[#ff6700]">Mock Interviews</p>
-						<p className="hover:text-[#ff6700]">Interview Bootcamp</p>
-						<p className="hover:text-[#ff6700]">Guides</p>
+					<div className="cursor-pointer flex gap-5 text-primary">
+						<p className="hover:text-[#ff6700] duration-300">Cources</p>
+						<p className="hover:text-[#ff6700] duration-300">Pricing</p>
+						<p className="hover:text-[#ff6700] duration-300">Mock Interviews</p>
+						<p className="hover:text-[#ff6700] duration-300">Interview Bootcamp</p>
+						<p className="hover:text-[#ff6700] duration-300">Guides</p>
 					</div>
 				</div>
-				<div className="cursor-pointer flex gap-2">
-					<p className="hover:text-[#ff6700]">Contact Us</p>
-					<p className="flex gap-1 hover:text-[#ff6700]">
+				<div className="cursor-pointer flex gap-5">
+					<p className="text-primary hover:text-[#ff6700] duration-300">Contact Us</p>
+					<p className="text-primary flex gap-1 hover:text-[#ff6700] duration-300">
 						<BiLogIn className="w-6 h-6" />
 						Login in
 					</p>
-					<p className="flex gap-1 hover:text-[#ff6700]">
+					<p className="text-primary flex gap-1 hover:text-[#ff6700] duration-300">
 						<LuUserPlus className="w-6 h-6" />
 						Sign Up
 					</p>
