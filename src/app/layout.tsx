7ee3abadcc2bb57a8ac246a8f4c69a0a0776fed6
Sk Shariful Islam <sksf211@gmail.com>
2023-11-18 03:04:05 +0600

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 
